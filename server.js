1285a7d1e2bc6c9863df0789bd584c5d6f138fe6
Dakota Jones <dakota32jones@gmail.com>
2022-08-28 17:54:50 -0500

    added delete function in server js for notes

--- server.js
+++ server.js
@@ -58,6 +58,24 @@ app.post("/api/notes", (req, res) => {
   res.json(newNote);
 });
 
+// adding delete function for notes
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+
+      break;
+    }
+  }
+}
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
